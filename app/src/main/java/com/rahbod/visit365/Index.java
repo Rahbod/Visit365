eaef431a2609ed4ed20077425dd6bb222fa5f8cf
behnam8871 <bgh1371@gmail.com>
2017-12-31 01:11:30 +0330

    create class: 1-RecyclerAdapter 2-Step1Activity 3-TransactionFragment create layout: 1-select_expertise 2-transactionfragment
    change:
    1-activity_index
    2-activity_step1

--- app/src/main/java/com/rahbod/visit365/Index.java
+++ app/src/main/java/com/rahbod/visit365/Index.java
@@ -1,36 +1,56 @@
 package com.rahbod.visit365;
 
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
 
-
-import com.rahbod.visit365.Fragment.Select_Dr;
+import java.util.ArrayList;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Index extends AppCompatActivity {
 
+    RecyclerAdapter adapter;
+    RecyclerView listExp;
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_step4);
+        setContentView(R.layout.activity_index);
 
-        Select_Dr select_dr = new Select_Dr();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_home, select_dr);
-        transaction.commit();
 
-        
+        ArrayList<String[]> expertises = new ArrayList<String[]>();
+        expertises.add(new String[]{"1", "قلب و عروق", "heart_index"});
+        expertises.add(new String[]{"2", "گوش و حلق و بینی", "listen_index"});
+        expertises.add(new String[]{"3", "چشم پزشکی", "eye_index"});
+        expertises.add(new String[]{"4", "مغز و اعصاب", "brain_index"});
+        expertises.add(new String[]{"5", "پوست و مو", "hair_index"});
+        expertises.add(new String[]{"6", "گوارش و کبد", "digestion_index"});
+        expertises.add(new String[]{"7", "دندان پزشکی", "dental_index"});
+        expertises.add(new String[]{"8", "ارتوپدی", "orthopedic_index"});
+        expertises.add(new String[]{"9", "ریه", "lung_index"});
+        expertises.add(new String[]{"10", "ارولوژی", "urology_index"});
+        expertises.add(new String[]{"11", "زنان و زایمان", "givingbirth_index"});
+        expertises.add(new String[]{"12", "روماتولوژی", "rheumatology_index"});
+
+        adapter = new RecyclerAdapter(expertises, this);
+
+        listExp = (RecyclerView) findViewById(R.id.recSelectExp);
+
+        listExp.setLayoutManager(new GridLayoutManager(this, 2));
+
+        listExp.setAdapter(adapter);
     }
 
     public void openNv(View view){
--- app/src/main/java/com/rahbod/visit365/RegisterActivity.java
+++ app/src/main/java/com/rahbod/visit365/RegisterActivity.java
@@ -8,6 +8,5 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
     }
 }
